791300af5f37f7ea2851103c0b7df99e95bad1a5
WalterJuniorBarreto <martin.barreto.com@gmail.com>
2026-03-25 15:17:45 -0500

    f

---
+++ DesSolNubeTest/conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+function conexion(){
+    $host = "dpg-d723d5oule4c73bhctbg-a.oregon-postgres.render.com";
+    $port = "port=5432";
+    $dbname = "dbname=name_postgres";
+    $user = "user=name_postgres_user";
+    $password = "password=TZRt7NNBptIDRnUCzpXgn1BKHunxoSmP";
+
+    $db = pg_connect("$host $port $dbname $user $password");
+
+    return $db;
+}
+
+?>
